12658bf20344bdc3e781e7a6b67a0356282a7257
pyother <piotr.dominik.sobol@gmail.com>
2024-05-31 14:08:10 +0200

    Main sketch -> rotation control

--- arduino/main_sketch/main_sketch.ino
+++ arduino/main_sketch/main_sketch.ino
@@ -23,6 +23,11 @@ int status = WL_IDLE_STATUS; // → status of connection.
 #define ENGINE2_BACKWARD 16
 #define SPEED1 5
 #define SPEED2 6
+#define ENKODER1 2
+#define ENKODER2 3
+
+volatile int count1 = 0;
+volatile int count2 = 0;
 
 void setup()
 {
@@ -36,6 +41,12 @@ void setup()
   pinMode(ENGINE2_BACKWARD, OUTPUT);
   pinMode(SPEED1, OUTPUT);
   pinMode(SPEED2, OUTPUT);
+
+  pinMode(4, INPUT);
+  pinMode(11, INPUT);
+
+  attachInterrupt(digitalPinToInterrupt(ENKODER1), engine1_counter, FALLING);
+  attachInterrupt(digitalPinToInterrupt(ENKODER2), engine2_counter, FALLING);
 
   // =======================================================
   // ↓ Connection loop.
@@ -142,9 +153,42 @@ void change_position(int position)
   }
 }
 
-void rotate(int angle)
-{
-  Serial.println("Rotation change request.");
-  Serial.print("Angle: ");
-  Serial.println(angle);
+void rotate(int angle) {
+
+  int ilosc_impulsow = 6 * angle;
+
+  count1 = 0;
+  count2 = 0;
+
+  analogWrite(SPEED1, 80);
+  analogWrite(SPEED2, 80);
+
+  digitalWrite(ENGINE1_BACKWARD, HIGH);
+  digitalWrite(ENGINE2_FORWARD, HIGH);
+
+  Serial.println(count1);
+
+  while (count1 > (-1) * ilosc_impulsow) {}
+
+  digitalWrite(ENGINE1_BACKWARD, LOW);
+  digitalWrite(ENGINE2_FORWARD, LOW);
+}
+
+void engine1_counter(){   // Odliczanie obrotów enkodera z wykorzystaniem przewań na pinie 2
+  if (digitalRead(4) == HIGH) {
+    count1--;
+  }
+  else {
+    count1++;
+  }
+  
+}
+
+void engine2_counter() {   // Odliczanie obrotów enkodera z wykorzystaniem przewań na pinie 2
+  if (digitalRead(11) == HIGH) {
+    count2++;
+  } 
+  else {
+    count2--;
+  }
 }
